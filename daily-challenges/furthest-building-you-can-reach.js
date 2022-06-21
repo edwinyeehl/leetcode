af4be0052d21108336869616e83e4aeca33caa39
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-06-22 02:27:25 +0800

    daily challenge 220621

---
+++ daily-challenges/furthest-building-you-can-reach.js
@@ -0,0 +1,49 @@
+/**
+ * @param {Array} queue
+ * @param {Number} num
+ */
+const add = (queue, num) => {
+    for(let i = 0; i < queue.length; i++) {
+        if(queue[i] > num) {
+          queue.splice(i, 0, num);
+          return;
+        }
+    }
+
+    queue.push(num);
+}
+
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+
+var furthestBuilding = function(heights, bricks, ladders) {
+    let queue = [];
+    let length = heights.length;
+
+    for (let i = 1; i < length; i++) {
+        let diff = heights[i] - heights[i - 1]
+        if (diff <= 0) {
+            continue;
+        }
+
+      add(queue, diff);
+      if (queue.length <= ladders) {
+          continue;
+      }
+
+      bricks -= queue[0];
+      if (bricks < 0) {
+          return i - 1;
+      }
+
+      queue.shift();
+    }
+
+    return length - 1;
+}
+
+console.log(furthestBuilding([4,2,7,6,9,14,12], 5, 1));
