e204aaed1af3370859b6107ec8f31da83d9949b9
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-06-22 22:28:05 +0800

    update to daily challenge

--- daily-challenges/kth-largest-element-in-an-array.js
+++ daily-challenges/kth-largest-element-in-an-array.js
@@ -6,12 +6,7 @@
 
 var findKthLargest = function(nums, k) {
     nums.sort((a, b) => b - a);
-    
-    for (let i = 0; i < k - 1; i++) {
-        nums.shift(); // shift removes the first element of the array
-    }
-
-    return nums[0]; // return the first element of the array
+    return nums[k - 1]; // return the k - 1 largest element of the array
 }
 
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
