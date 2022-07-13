4d4e80ac65a53fb6d67c2b5ae8f88f44e1411425
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-07-13 22:59:02 +0800

    leetcode 75 day 7

---
+++ LeetCode-75-lvl-1/binary-search.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function(nums, target) {
+    // basically just return nums.indexOf(target)
+    // but in a more complex way
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
