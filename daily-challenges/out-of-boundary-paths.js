24b59c9bc30eb54c8c6ddce5d40c522c28a06a5f
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-07-17 00:22:38 +0800

    update for readability

--- daily-challenges/out-of-boundary-paths.js
+++ daily-challenges/out-of-boundary-paths.js
@@ -4,22 +4,30 @@
  * @param {number} maxMove
  * @param {number} startRow
  * @param {number} startColumn
+ * @param {Map} memo - optional
  * @return {number}
  */
 
 var findPaths = function(m, n, maxMove, startRow, startColumn, memo = new Map()) {
     const key = maxMove + ',' + startRow + ',' + startColumn;
-    if (memo.has(key)) return memo.get(key);
+
+    if (memo.has(key))
+        return memo.get(key);
+    
     const isOutside = startRow === -1 || startRow === m || startColumn === -1 || startColumn === n;
-    if (maxMove === 0 || isOutside) return +isOutside;
+
+    if (maxMove === 0 || isOutside)
+        return +isOutside;
+
     memo.set(key, (
           findPaths(m, n, maxMove - 1, startRow - 1, startColumn, memo)
         + findPaths(m, n, maxMove - 1, startRow + 1, startColumn, memo)
         + findPaths(m, n, maxMove - 1, startRow, startColumn + 1, memo)
         + findPaths(m, n, maxMove - 1, startRow, startColumn - 1, memo)
-    ) % 1000000007);
+    ) % 1000000007); // modulo 1000000007 to avoid overflow
 
     return memo.get(key);
 }
 
 console.log(findPaths(1, 3, 3, 0, 1)); // 12
+console.log(findPaths(2, 2, 2, 0, 0)); // 6
