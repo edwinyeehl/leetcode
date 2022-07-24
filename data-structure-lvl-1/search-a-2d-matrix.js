d234b66f11c9966dce88147d906a1d72ee17c7a1
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-07-24 12:40:24 +0800

    daily challenge 220724

--- data-structure-lvl-1/search-a-2d-matrix.js
+++ data-structure-lvl-1/search-a-2d-matrix.js
@@ -4,18 +4,32 @@
  * @return {boolean}
  */
  var searchMatrix = function(matrix, target) {
-    var row = 0, col = matrix[0].length - 1;
-    while (row < matrix.length && col >= 0) {
-        if (matrix[row][col] === target) {
-            return true;
-        } else if (matrix[row][col] > target) {
-            col--;
+    if (!matrix || !matrix.length) return false;
+
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    const findTarget = (startRow, endRow, startCol, endCol) => {
+        if (startRow > endRow || startCol > endCol) return false;
+
+        const midRow = Math.floor((endRow - startRow) / 2) + startRow;
+        const midCol = Math.floor((endCol - startCol) / 2) + startCol;
+
+        const midVal = matrix[midRow][midCol];
+
+        if (midVal === target) return true;
+
+        if (midVal > target) {
+            return findTarget(startRow, endRow, startCol, midCol - 1) || findTarget(startRow, midRow - 1, midCol, endCol);
         } else {
-            row++;
+            return findTarget(midRow + 1, endRow, startCol, endCol) || findTarget(startRow, midRow, midCol + 1, endCol);
         }
     }
-    return false;
+
+    return findTarget(0, rows - 1, 0, cols - 1);
 }
 
 console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
 console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+console.log(searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5))
+console.log(searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 20))
