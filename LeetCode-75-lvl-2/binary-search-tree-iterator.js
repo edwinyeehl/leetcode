4b9eb335c9b57b7cd9dd6cc07f00173059644c94
Edwin Yee <9399459+edwinyeehl@users.noreply.github.com>
2022-07-29 20:21:01 +0800

    cosmetic change

--- LeetCode-75-lvl-2/binary-search-tree-iterator.js
+++ LeetCode-75-lvl-2/binary-search-tree-iterator.js
@@ -4,9 +4,9 @@
 
 class TreeNode {
   constructor(val, left, right) {
-      this.val = (val===undefined ? 0 : val)
-      this.left = (left===undefined ? null : left)
-      this.right = (right===undefined ? null : right)
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
   }
 }
 
